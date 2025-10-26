9c4f5e8d30f7b4a49417bbca3331c65dc1ec3f8b
mukul porwal <mukulporwal05@gmail.com>
2025-10-26 23:19:28 +0530

    fixed static error

--- eventweb/settings.py
+++ eventweb/settings.py
@@ -126,9 +126,8 @@ USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
